a716ad7383d85f8ad8347c434b35bd4b40e24df0
Enry Goncalves <enry_lucas@hotmail.com>
2019-03-22 19:10:21 -0300

    first commit

---
+++ src/main/java/enry/snackbar/SnackBarApplication.java
@@ -0,0 +1,13 @@
+package enry.snackbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enry/snackbar/SnackBarApplicationTests.java
@@ -0,0 +1,16 @@
+package enry.snackbar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnackBarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
