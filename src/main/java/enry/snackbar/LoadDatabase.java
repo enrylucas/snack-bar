d789fbe79f3440f99a216051c77f60a85d0603b7
Enry Goncalves <enry_lucas@hotmail.com>
2019-03-28 12:20:11 -0300

    snack-bar doc added

--- src/main/java/enry/snackbar/LoadDatabase.java
+++ src/main/java/enry/snackbar/LoadDatabase.java
@@ -16,43 +16,43 @@ import org.springframework.context.annotation.Configuration;
 public class LoadDatabase {
 
     @Bean
-    CommandLineRunner initDatabase(IngredientRepository repository, FoodRepository repository2) {
+    CommandLineRunner initDatabase(IngredientRepository ingredientRepository, FoodRepository foodRepository) {
         return args -> {
             Ingredient lettuce = new Ingredient("Alface", 0.40f, false, false);
             Ingredient bacon = new Ingredient("Bacon", 2.00f, false, false);
             Ingredient meatHamburguer = new Ingredient("Hamburguer de Carne", 3.00f, true, false);
             Ingredient egg = new Ingredient("Ovo", 0.80f, false, false);
             Ingredient cheese = new Ingredient("Queijo", 1.50f, false, true);
-            log.info("Preloading " + repository.save(lettuce));
-            log.info("Preloading " + repository.save(bacon));
-            log.info("Preloading " + repository.save(meatHamburguer));
-            log.info("Preloading " + repository.save(egg));
-            log.info("Preloading " + repository.save(cheese));
+            log.info("Preloading " + ingredientRepository.save(lettuce));
+            log.info("Preloading " + ingredientRepository.save(bacon));
+            log.info("Preloading " + ingredientRepository.save(meatHamburguer));
+            log.info("Preloading " + ingredientRepository.save(egg));
+            log.info("Preloading " + ingredientRepository.save(cheese));
             
             
             List<Ingredient> ingXBacon = new ArrayList<>();
             ingXBacon.add(bacon);
             ingXBacon.add(meatHamburguer);
             ingXBacon.add(cheese);
-            log.info("Preloading " + repository2.save(new Food("X-Bacon", ingXBacon)));
+            log.info("Preloading " + foodRepository.save(new Food("X-Bacon", ingXBacon)));
             
             List<Ingredient> ingXBurguer = new ArrayList<>();
             ingXBurguer.add(meatHamburguer);
             ingXBurguer.add(cheese);
-            log.info("Preloading " + repository2.save(new Food("X-Burguer", ingXBurguer)));
+            log.info("Preloading " + foodRepository.save(new Food("X-Burguer", ingXBurguer)));
             
             List<Ingredient> ingXEgg = new ArrayList<>();
             ingXEgg.add(egg);
             ingXEgg.add(meatHamburguer);
             ingXEgg.add(cheese);
-            log.info("Preloading " + repository2.save(new Food("X-Egg", ingXEgg)));
+            log.info("Preloading " + foodRepository.save(new Food("X-Egg", ingXEgg)));
             
             List<Ingredient> ingXEggBacon = new ArrayList<>();
             ingXEggBacon.add(egg);
             ingXEggBacon.add(bacon);
             ingXEggBacon.add(meatHamburguer);
             ingXEggBacon.add(cheese);
-            log.info("Preloading " + repository2.save(new Food("X-Egg Bacon", ingXEggBacon)));
+            log.info("Preloading " + foodRepository.save(new Food("X-Egg Bacon", ingXEggBacon)));
             
         };
     }
--- src/test/java/enry/snackbar/SnackBarApplicationTests.java
+++ src/test/java/enry/snackbar/SnackBarApplicationTests.java
@@ -1,6 +1,5 @@
 package enry.snackbar;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import enry.snackbar.controller.FoodController;
@@ -16,6 +15,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,92 +30,117 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 //@AutoConfigureWebTestClient
 public class SnackBarApplicationTests {
 
+    private MockMvc ingredientMvc;
+    private MockMvc foodMvc;
+
+    @Mock
+    private IngredientRepository ingredientRepository;
+
+    @Mock
+    private FoodRepository foodRepository;
+
+    @InjectMocks
+    private IngredientController ingredientController;
+
+    @InjectMocks
+    private FoodController foodController;
+
+    @Before
+    public void setUp() {
+        ingredientMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+        foodMvc = MockMvcBuilders.standaloneSetup(foodController).build();
+
+    }
+
+    @Test
+    public void getIngredientsTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/ingredients");
+        ingredientMvc.perform(request).andExpect(status().isOk());
+    }
+
+    @Test
+    public void getFoodsTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/foods");
+        foodMvc.perform(request).andExpect(status().isOk());
+    }
+
+    @Test
+    public void getPriceTest() throws Exception {
+        IngredientWrapper wrapper = new IngredientWrapper();
+        List<Integer> ings = new ArrayList<>();//X-Bacon
+        ings.add(2);
+        ings.add(3);
+        ings.add(5);
+        wrapper.setIngredientsId(ings);
+        List<Integer> qts = new ArrayList<>();
+        qts.add(1);
+        qts.add(1);
+        qts.add(1);
+        wrapper.setIngredientsQty(qts);
+
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(wrapper);
         
-        private MockMvc ingredientMvc;
-        private MockMvc foodMvc;
+        Ingredient lettuce = new Ingredient("Alface", 0.40f, false, false);
+        Ingredient bacon = new Ingredient("Bacon", 2.00f, false, false);
+        Ingredient meatHamburguer = new Ingredient("Hamburguer de Carne", 3.00f, true, false);
+        Ingredient egg = new Ingredient("Ovo", 0.80f, false, false);
+        Ingredient cheese = new Ingredient("Queijo", 1.50f, false, true);
+
+        String lettuce2 = ow.writeValueAsString(lettuce);
+        RequestBuilder request1 = MockMvcRequestBuilders.post("/ingredients").contentType(MediaType.APPLICATION_JSON).content(lettuce2);
+        ingredientMvc.perform(request1);
+        String bacon2 = ow.writeValueAsString(bacon);
+        RequestBuilder request2 = MockMvcRequestBuilders.post("/ingredients").contentType(MediaType.APPLICATION_JSON).content(bacon2);
+        ingredientMvc.perform(request2);
+        String meatHamburguer2 = ow.writeValueAsString(meatHamburguer);
+        RequestBuilder request3 = MockMvcRequestBuilders.post("/ingredients").contentType(MediaType.APPLICATION_JSON).content(meatHamburguer2);
+        ingredientMvc.perform(request3);
+        String egg2 = ow.writeValueAsString(egg);
+        RequestBuilder request4 = MockMvcRequestBuilders.post("/ingredients").contentType(MediaType.APPLICATION_JSON).content(egg2);
+        ingredientMvc.perform(request4);
+        String cheese2 = ow.writeValueAsString(cheese);
+        RequestBuilder request5 = MockMvcRequestBuilders.post("/ingredients").contentType(MediaType.APPLICATION_JSON).content(cheese2);
+        ingredientMvc.perform(request5);
         
-        @Mock
-        private IngredientRepository ingredientRepository;
-        
-        @Mock
-        private FoodRepository foodRepository;
-        
-        @InjectMocks
-        private IngredientController ingredientController;
-        
-        @InjectMocks
-        private FoodController foodController;
-    
-        @Before
-        public void setUp() {
-            ingredientMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
-            foodMvc = MockMvcBuilders.standaloneSetup(foodController).build();
-        }
-        
-	@Test
-	public void getIngredientsTest() throws Exception {
-            RequestBuilder request = MockMvcRequestBuilders.get("/ingredients");
-            ingredientMvc.perform(request).andExpect(status().isOk());
-	}
-        
-        @Test
-	public void getFoodsTest() throws Exception {
-            RequestBuilder request = MockMvcRequestBuilders.get("/foods");
-            foodMvc.perform(request).andExpect(status().isOk());
-	}
-        
-        @Test
-	public void getPriceTest() throws Exception {
-            IngredientWrapper wrapper = new IngredientWrapper();
-            List<Integer> ings = new ArrayList<>();//X-Bacon
-            ings.add(2);
-            ings.add(3);
-            ings.add(5);
-            wrapper.setIngredientsId(ings);
-            List<Integer> qts = new ArrayList<>();
-            qts.add(1);
-            qts.add(1);
-            qts.add(1);
-            wrapper.setIngredientsQty(qts);
-            
-            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-            String json = ow.writeValueAsString(wrapper);
-            RequestBuilder request = MockMvcRequestBuilders.post("/ingredients/build").contentType(MediaType.APPLICATION_JSON).content(json);
-            ingredientMvc.perform(request).andExpect(content().string("6.50"));
-	}
-        
-        @Test
-	public void getPriceTestWithInflation() throws Exception {
-            IngredientWrapper wrapper = new IngredientWrapper();
-            List<Integer> ings = new ArrayList<>();//X-Egg Bacon
-            ings.add(2);
-            ings.add(3);
-            ings.add(4);
-            ings.add(5);
-            wrapper.setIngredientsId(ings);
-            List<Integer> qts = new ArrayList<>();
-            qts.add(1);
-            qts.add(1);
-            qts.add(1);
-            qts.add(1);
-            wrapper.setIngredientsQty(qts);
-            
-            Ingredient newBacon = new Ingredient();
-            newBacon.setId(2);
-            newBacon.setName("Bacon");
-            newBacon.setPrice(4.0f);
-            
-            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-            String json = ow.writeValueAsString(wrapper);
-            String newPrice = ow.writeValueAsString(newBacon);
-            
-            
-            RequestBuilder request1 = MockMvcRequestBuilders.put("/ingredients/2").contentType(MediaType.APPLICATION_JSON_UTF8).content(newPrice);
-            ingredientMvc.perform(request1).andExpect(status().isOk()); //isso realmente altera o valor no banco? (eu gostaria que sim)
-            
-            
-            RequestBuilder request2 = MockMvcRequestBuilders.post("/ingredients/build").contentType(MediaType.APPLICATION_JSON_UTF8).content(json);
-            ingredientMvc.perform(request2).andExpect(content().string("9.30")); //o findById dentro do post não funciona quando é o teste rodando
-	}
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/ingredients/build").contentType(MediaType.APPLICATION_JSON).content(json);
+        ingredientMvc.perform(request).andExpect(content().string("6.50"));
+    }
+
+    @Test
+    public void getPriceTestWithInflation() throws Exception {
+        IngredientWrapper wrapper = new IngredientWrapper();
+        List<Integer> ings = new ArrayList<>();//X-Egg Bacon
+        ings.add(2);
+        ings.add(3);
+        ings.add(4);
+        ings.add(5);
+        wrapper.setIngredientsId(ings);
+        List<Integer> qts = new ArrayList<>();
+        qts.add(1);
+        qts.add(1);
+        qts.add(1);
+        qts.add(1);
+        wrapper.setIngredientsQty(qts);
+
+        Ingredient newBacon = new Ingredient();
+        newBacon.setId(2);
+        newBacon.setName("Bacon");
+        newBacon.setPrice(4.0f);
+
+        ingredientRepository.findById(newBacon.getId()).map(ingredient -> {
+            ingredient.setPrice(newBacon.getPrice());
+            System.out.println("New Ingredient:");
+            System.out.println(ingredient);
+            return ingredientRepository.save(ingredient);
+        });
+
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(wrapper);
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/ingredients/build").contentType(MediaType.APPLICATION_JSON_UTF8).content(json);
+        ingredientMvc.perform(request).andExpect(content().string("9.30")); //o findById dentro do post não funciona quando é o teste rodando
+    }
 
 }
